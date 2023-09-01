2306045df9ab05dbb9d9f2eb01b02db3625033e8
revisto <therevisto@gmail.com>
2023-09-02 00:43:40 +0430

    update config

--- config.py
+++ config.py
@@ -1,13 +1,9 @@
 import os
 from dotenv import load_dotenv
-import sys
 
-def get_api_token():
-    load_dotenv()  # Load variables from .env file
-    api_token = os.getenv('API_TOKEN')
-    
-    if not api_token:
-        print('API token not found in .env file.')
-        sys.exit(1)
 
-    return api_token
+load_dotenv()
+
+API_TOKEN = os.getenv('API_TOKEN')
+ADMIN_TELEGRAM_ID = os.getenv('ADMIN_TELEGRAM_ID')
+TARGET_TELEGRAM_ID = os.getenv('TARGET_TELEGRAM_ID')
