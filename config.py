041239f0c890fd82dfffb31c147e0454ae7b3866
revisto <therevisto@gmail.com>
2023-09-02 00:12:46 +0430

    add token validation

--- config.py
+++ config.py
@@ -5,10 +5,9 @@ import sys
 def get_api_token():
     load_dotenv()  # Load variables from .env file
     api_token = os.getenv('API_TOKEN')
+    
+    if not api_token:
+        print('API token not found in .env file.')
+        sys.exit(1)
+
     return api_token
-
-token = get_api_token()
-
-if not token:
-    print('API token not found in .env file.')
-    sys.exit(1)
