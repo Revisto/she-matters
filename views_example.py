fa5180d69e750f13ec3a27ed2f569bbb7a490bc8
revisto <therevisto@gmail.com>
2023-09-02 03:21:46 +0430

    add an example for views

---
+++ views_example.py
@@ -0,0 +1,53 @@
+from random import choice
+
+class ReminderTexts:
+    
+    def meds_before_breakfast_text():
+        message = """"""
+        return message
+
+    def meds_12_hour_morning_text():
+        message = """"""
+        return message
+
+    def meds_12_hour_night_text():
+        message = """"""
+        return message
+
+    def meds_8_hour_text():
+        message = """"""
+        return message
+    
+    def meds_6_hour_text():
+        message = """"""
+        return message
+
+    def meds_meanwhile_launch():
+        message = """"""
+        return message
+
+    def meds_before_dinner():
+        message = """"""
+        return message
+
+    def meds_meanwhile_dinner():
+        message = """"""
+        return message
+    
+    def meds_night():
+        message = """"""
+        return message
+    
+    def took_medicine():
+        return ""
+    
+    def snooze(snooze_time):
+        return f""
+    
+    def beep():
+        messages = [""]
+        return choice(messages)
+    
+    def start():
+        message = "She Matters :)"
+        return message
