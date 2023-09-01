72e7e1f5cf74515967b4755d67785df4804657e3
revisto <therevisto@gmail.com>
2023-09-02 03:20:06 +0430

    add main

---
+++ main.py
@@ -0,0 +1,10 @@
+import threading
+import time
+import schedule
+from handlers import bot
+
+if __name__ == '__main__':
+    threading.Thread(target=bot.infinity_polling, name='bot_infinity_polling', daemon=True).start()
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
