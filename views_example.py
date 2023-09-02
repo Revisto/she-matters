9e0b93f70bade98c34a424c01a6f3689048fb4f3
revisto <therevisto@gmail.com>
2023-09-02 19:56:12 +0430

    add message tags

--- views_example.py
+++ views_example.py
@@ -1,53 +1,67 @@
 from random import choice
 
 class ReminderTexts:
+
+    def messages_tag(self):
+        messages_tag = {
+            self.meds_before_breakfast_text(): "meds_before_breakfast_text", 
+            self.meds_12_hour_morning_text(): "meds_12_hour_morning_text", 
+            self.meds_12_hour_night_text(): "meds_12_hour_night_text", 
+            self.meds_8_hour_text(): "meds_8_hour_text", 
+            self.meds_6_hour_text(): "meds_6_hour_text", 
+            self.meds_meanwhile_launch(): "meds_meanwhile_launch", 
+            self.meds_before_dinner(): "meds_before_dinner", 
+            self.meds_meanwhile_dinner(): "meds_meanwhile_dinner", 
+            self.meds_night(): "meds_night", 
+        }
+        return messages_tag
     
-    def meds_before_breakfast_text():
+    def meds_before_breakfast_text(self):
         message = """"""
         return message
 
-    def meds_12_hour_morning_text():
+    def meds_12_hour_morning_text(self):
         message = """"""
         return message
 
-    def meds_12_hour_night_text():
+    def meds_12_hour_night_text(self):
         message = """"""
         return message
 
-    def meds_8_hour_text():
-        message = """"""
-        return message
-    
-    def meds_6_hour_text():
-        message = """"""
-        return message
-
-    def meds_meanwhile_launch():
-        message = """"""
-        return message
-
-    def meds_before_dinner():
-        message = """"""
-        return message
-
-    def meds_meanwhile_dinner():
+    def meds_8_hour_text(self):
         message = """"""
         return message
     
-    def meds_night():
+    def meds_6_hour_text(self):
+        message = """"""
+        return message
+
+    def meds_meanwhile_launch(self):
+        message = """"""
+        return message
+
+    def meds_before_dinner(self):
+        message = """"""
+        return message
+
+    def meds_meanwhile_dinner(self):
         message = """"""
         return message
     
-    def took_medicine():
+    def meds_night(self):
+        message = """"""
+        return message
+    
+    def took_medicine(self):
         return ""
     
     def snooze(snooze_time):
         return f""
     
-    def beep():
+    def beep(self):
         messages = [""]
         return choice(messages)
     
-    def start():
+    def start(self):
         message = "She Matters :)"
         return message
